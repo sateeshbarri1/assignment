f18cde944c162a0e905d330e912dc107b9dffbeb
sateesh <sateeshbarri1@gmail.com>
2025-02-12 14:49:34 +0530

    changer browser Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import axios from "axios";
 import "tailwindcss/tailwind.css";
 import Login from "./components/Login";
